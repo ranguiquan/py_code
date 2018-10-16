9dc6e35d25cc6c481485f85b39f290921e028b26
ranguiquan <673227233@qq.com>
2018-10-16 19:39:12 +0800

    first commit

---
+++ leetcode/2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+def example():
+    a = '3'
+    b = 2
+    print(globals())
+    print(locals())
+
+x = 5
+example()
---
+++ test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+first = '3'
+second = '4'
+print(first)
